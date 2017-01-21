3bb87823c1475a0228fdc6d63a8dc3000acf3913
Jesus Imery <yisas_0929@hotmail.com>
2017-01-21 16:14:05 -0600

    working on the bomb, using sphere overlap and expanding slowable base class

---
+++ Assets/Scripts/Objects/Bomb.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float aoeRadius;
+    public float slowdownMultiplier;
+
+    // Private references
+    SphereCollider aoe;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Collider[] collisions = Physics.OverlapSphere(transform.position, aoeRadius);
+
+        foreach (Collider hit in collisions)
+        {
+            if(hit.tag == "Slowable")
+            {
+                hit.gameObject.GetComponent<Slowable>().Slow(slowdownMultiplier);
+            }
+        }
+    }
+
+    void Explode()
+    {
+
+    }
+}
--- Assets/Scripts/Objects/Slowable.cs
+++ Assets/Scripts/Objects/Slowable.cs
@@ -4,4 +4,14 @@ public class Slowable : MonoBehaviour
 {
     [HideInInspector]
     public float fictitiousTimeScale = 1;
+
+    // State variables
+    private bool slowed;
+
+    // Called from external script to slow down this object
+    public void Slow(float slowdownMultiplier)
+    {
+        fictitiousTimeScale *= slowdownMultiplier;
+    }
+
 }
