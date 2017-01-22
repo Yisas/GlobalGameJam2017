7ab81739219fc0371e9cd18d3305dad1148617ab
hani-sayegh <papaaced@gmail.com>
2017-01-22 12:37:24 -0600

    fix bugs

--- Assets/vel.cs
+++ Assets/vel.cs
@@ -6,6 +6,9 @@ public class vel : MonoBehaviour
 {
     void Start()
     {
+        transform.position = Camera.main.transform.position;
+        //print(transform.position);
+        //Debug.Break();
         GetComponent<Rigidbody>().velocity = transform.forward * 10;
     }
 }
