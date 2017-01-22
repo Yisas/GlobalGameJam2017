ed0f2966f3a397904ad1942d84ec4e73d1b93586
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-01-22 12:26:24 -0600

    Added health and dead, floating island and harming obstacles

--- Assets/Scripts/Miscellaneous/ParentCollider.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public interface ParentCollider
-{
-    // A script that inherits from  this interface can recieve collision calls from an object with a ChildCollider script.
-    void OnTriggerEnterFromChild(Collider col);
-    void OnCollisionEnterFromChild(Collision col);
-}
