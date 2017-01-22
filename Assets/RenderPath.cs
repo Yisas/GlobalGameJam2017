fa8c26540bf03ab0f3168069d6a1395ed08d8ea1
hani <hani>
2017-01-21 21:18:41 -0600

    start visulizer

---
+++ Assets/RenderPath.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderPath : MonoBehaviour
+{
+    public float initialVelocity = 10.0f;
+    public float timeResolution = 0.02f;
+    public float maxTime = 10.0f;
+
+    private LineRenderer lineRenderer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocityVector = transform.forward * initialVelocity;
+        lineRenderer.SetVertexCount((int)(maxTime / timeResolution));
+
+        int index = 0;
+        Vector3 currentPosition = transform.position;
+        for (float t = 0.0f; t < maxTime; t += timeResolution)
+        {
+            lineRenderer.SetPosition(index, currentPosition);
+            currentPosition += velocityVector * timeResolution;
+            ++index;
+        }
+    }
+}
