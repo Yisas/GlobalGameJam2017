2534cb3c22fa22b1eb19bbbc1334b936a536ceb9
Jesus Imery <yisas_0929@hotmail.com>
2017-01-21 23:46:52 -0600

    Ninja ground player

---
+++ Assets/Scripts/Flying Character/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = offset + player.transform.position;
+    }
+}
